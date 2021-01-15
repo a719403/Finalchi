619bd5e83162568b8f7e0d6a26f26a43c5bbe46a
a719403 <71936352+a719403@users.noreply.github.com>
2021-01-15 16:58:46 +0800

    Add files via upload

---
+++ js/hover.js
@@ -0,0 +1,105 @@
+$(document).ready(function(){  
+    $("#p000001").hover(function(){  
+      $(this).attr('src','./img/colorful_img/000001.jpg');  
+      }, function(){  
+      $(this).attr('src','./img/flim_img/000001.jpg');  
+    });
+
+$(document).ready(function(){  
+    $("#p000002").hover(function(){  
+        $(this).attr('src','./img/colorful_img/000002.jpg');  
+        }, function(){  
+        $(this).attr('src','./img/flim_img/000001.jpg');  
+    });
+});
+    
+$(document).ready(function(){  
+    $("#p000003").hover(function(){  
+        $(this).attr('src','img/colorful_img/000003.jpg');  
+        }, function(){  
+            $(this).attr('src','img/flim_img/000001.jpg');  
+        });
+    });
+
+$(document).ready(function(){  
+        $("#p000004").hover(function(){  
+            $(this).attr('src','img/colorful_img/000004.jpg');  
+            }, function(){  
+                $(this).attr('src','img/flim_img/000001.jpg');  
+            });
+});
+
+$(document).ready(function(){  
+        $("#p000005").hover(function(){  
+            $(this).attr('src','img/colorful_img/000005.jpg');  
+            }, function(){   
+            $(this).attr('src','img/flim_img/000001.jpg');  
+            });
+});
+
+$(document).ready(function(){  
+    $("#p000006").hover(function(){  
+        $(this).attr('src','img/colorful_img/000006.jpg');  
+        }, function(){        
+        $(this).attr('src','img/flim_img/000001.jpg');  
+        });
+});
+
+$(document).ready(function(){  
+    $("#p000007").hover(function(){  
+        $(this).attr('src','img/colorful_img/000007.jpg');  
+        }, function(){        
+        $(this).attr('src','img/flim_img/000001.jpg');  
+        });
+});
+
+$(document).ready(function(){  
+    $("#p000008").hover(function(){  
+        $(this).attr('src','img/colorful_img/000008.jpg');  
+        }, function(){        
+        $(this).attr('src','img/flim_img/000001.jpg');  
+        });
+});
+
+$(document).ready(function(){  
+    $("#p000009").hover(function(){  
+        $(this).attr('src','img/colorful_img/000009.jpg');  
+        }, function(){        
+        $(this).attr('src','img/flim_img/000001.jpg');  
+        });
+});
+
+$(document).ready(function(){  
+    $("#p000010").hover(function(){  
+        $(this).attr('src','img/colorful_img/000010.jpg');  
+        }, function(){        
+        $(this).attr('src','img/flim_img/000001.jpg');  
+        });
+});
+
+//     標籤的
+    $("#label_PEN").hover(function(){  
+        $(this).attr('src','./img/PEN HOVER.png');  
+        }, function(){  
+        $(this).attr('src','img/PEN unhover.png');  
+
+    });
+
+    $("#label_SIMPLE").hover(function(){  
+        $(this).attr('src','./img/SIMPLE HOVER.png');  
+        }, function(){  
+        $(this).attr('src','img/SIMPLE unhover.png');  
+    });
+
+});
+
+
+/* <img class="img1_size" id="" src="./flim_img/koda/壓縮/000002.jpg" alt="">
+<img class="img1_size" id="" src="./flim_img/koda/壓縮/000018.jpg" alt="">
+<img class="img1_size" id="" src="./flim_img/koda/壓縮/000019.jpg" alt="">
+<img class="img1_size" id="" src="./flim_img/koda/壓縮/000020.jpg" alt="">
+<img class="img1_size" id="" src="./flim_img/koda/壓縮/000023.jpg" alt="">
+<img class="img1_size" id="" src="./flim_img/koda/壓縮/000026.jpg" alt="">
+<img class="img1_size" id="" src="./flim_img/koda/壓縮/000030.jpg" alt="">
+<img class="img1_size" id="" src="./flim_img/koda/壓縮/000032.jpg" alt="">
+<img class="img1_size" id="" src="./flim_img/koda/壓縮/000038.jpg" alt=""> */
---
+++ js/pix_hover.js
@@ -0,0 +1,199 @@
+$(document).ready(function(){  
+    $("#p000001").hover(function(){  
+      $(this).attr('src','./img/colorful_img/koda200/壓縮/000001.jpg');  
+      }, function(){  
+      $(this).attr('src','./img/flim_img/koda200/壓縮/000001.jpg');  
+    });
+    
+    $("#p000002").hover(function(){  
+        $(this).attr('src','./img/colorful_img/koda200/壓縮/000002.jpg');  
+        }, function(){  
+        $(this).attr('src','./img/flim_img/koda200/壓縮/000002.jpg');  
+      });
+
+
+      $("#p000018").hover(function(){  
+        $(this).attr('src','./img/colorful_img/koda200/壓縮/000018.jpg');  
+        }, function(){  
+        $(this).attr('src','./img/flim_img/koda200/壓縮/000018.jpg');  
+      });
+
+      $("#p000019").hover(function(){  
+        $(this).attr('src','./img/colorful_img/koda200/壓縮/000019.jpg');  
+        }, function(){  
+        $(this).attr('src','./img/flim_img/koda200/壓縮/000019.jpg');  
+      });
+
+      $("#p000020").hover(function(){  
+        $(this).attr('src','./img/colorful_img/koda200/壓縮/000020.jpg');  
+        }, function(){  
+        $(this).attr('src','./img/flim_img/koda200/壓縮/000020.jpg');  
+      });
+
+      $("#p000023").hover(function(){  
+        $(this).attr('src','./img/colorful_img/koda200/壓縮/000023.jpg');  
+        }, function(){  
+        $(this).attr('src','./img/flim_img/koda200/壓縮/000023.jpg');  
+      });
+
+      $("#p000026").hover(function(){  
+        $(this).attr('src','./img/colorful_img/koda200/壓縮/000026.jpg');  
+        }, function(){  
+        $(this).attr('src','./img/flim_img/koda200/壓縮/000026.jpg');  
+      });
+
+      $("#p000030").hover(function(){  
+        $(this).attr('src','./img/colorful_img/koda200/壓縮/000030.jpg');  
+        }, function(){  
+        $(this).attr('src','./imgflim_img/koda200/壓縮/000030.jpg');  
+      });
+
+      $("#p000032").hover(function(){  
+        $(this).attr('src','./img/colorful_img/koda200/壓縮/000032.jpg');  
+        }, function(){  
+        $(this).attr('src','./img/flim_img/koda/壓縮200/000032.jpg');  
+      });
+      
+      $("#p000038").hover(function(){  
+        $(this).attr('src','./img/colorful_img/koda200/壓縮/000038.jpg');  
+        }, function(){  
+        $(this).attr('src','./img/flim_img/koda200/壓縮/000038.jpg');  
+      });
+
+    // 500t的
+    
+    $("#2_p000007").hover(function(){  
+      $(this).attr('src','./img/colorful_img/500t/壓縮/000007.jpg');  
+      }, function(){  
+      $(this).attr('src','./img/flim_img/500t/壓縮/000007.jpg');  
+    });
+    
+    $("#2_p000008").hover(function(){  
+      $(this).attr('src','./img/colorful_img/500t/壓縮/000008.jpg');  
+      }, function(){  
+      $(this).attr('src','./img/flim_img/500t/壓縮/000008.jpg');  
+    });
+
+    $("#2_p000009").hover(function(){  
+      $(this).attr('src','./img/colorful_img/500t/壓縮/000009.jpg');  
+      }, function(){  
+      $(this).attr('src','./img/flim_img/500t/壓縮/000009.jpg');  
+    });
+
+    $("#2_p000015").hover(function(){  
+      $(this).attr('src','./img/colorful_img/500t/壓縮/000015.jpg');  
+      }, function(){  
+      $(this).attr('src','./img/flim_img/500t/壓縮/000015.jpg');  
+    });
+
+    $("#2_p000016").hover(function(){  
+      $(this).attr('src','./img/colorful_img/500t/壓縮/0000016.jpg');  
+      }, function(){  
+      $(this).attr('src','./img/flim_img/500t/壓縮/000016.jpg');  
+    });
+
+    $("#2_p000021").hover(function(){  
+      $(this).attr('src','./img/colorful_img/500t/壓縮/000021.jpg');  
+      }, function(){  
+      $(this).attr('src','./img/flim_img/500t/壓縮/000021.jpg');  
+    });
+
+    $("#2_p0000024").hover(function(){  
+      $(this).attr('src','./img/colorful_img/500t/壓縮/000024.jpg');  
+      }, function(){  
+      $(this).attr('src','./img/flim_img/500t/壓縮/000024.jpg');  
+    });
+
+    $("#2_p000025").hover(function(){  
+      $(this).attr('src','./img/colorful_img/500t/壓縮/000025.jpg');  
+      }, function(){  
+      $(this).attr('src','./img/flim_img/500t/壓縮/000025.jpg');  
+    });
+
+    $("#2_p000036").hover(function(){  
+      $(this).attr('src','./img/colorful_img/500t/壓縮/000036.jpg');  
+      }, function(){  
+      $(this).attr('src','./img/flim_img/500t/壓縮/000036.jpg');  
+    });
+
+    $("#2_p000037").hover(function(){  
+      $(this).attr('src','./img/colorful_img/500t/壓縮/000037.jpg');  
+      }, function(){  
+      $(this).attr('src','./img/flim_img/500t/壓縮/000037.jpg');  
+    });
+
+
+      //   lomo的
+
+    $("#000267090003").hover(function(){  
+      $(this).attr('src','./img/colorful_img/lomo/壓縮/000267090003.jpg');  
+      }, function(){  
+      $(this).attr('src','./img/flim_img/lomo/壓縮/000267090003.jpg');  
+    });
+
+      $("#p000008").hover(function(){  
+        $(this).attr('src','./img/flim_img/500t/壓縮/000037.jpg');  
+        }, function(){  
+        $(this).attr('src','./flim_img/500t/壓縮/000008.jpg');  
+      });
+
+      $("#p000009").hover(function(){  
+        $(this).attr('src','./colorful_img/500t/壓縮/000009.jpg');  
+        }, function(){  
+        $(this).attr('src','./flim_img/500t/壓縮/000009.jpg');  
+      });  
+      
+      $("#p000015").hover(function(){  
+        $(this).attr('src','./colorful_img/500t/壓縮/000015.jpg');  
+        }, function(){  
+        $(this).attr('src','./flim_img/500t/壓縮/000015.jpg');  
+      });
+
+      $("#p000016").hover(function(){  
+        $(this).attr('src','./colorful_img/500t/壓縮/000016.jpg');  
+        }, function(){  
+        $(this).attr('src','./flim_img/500t/壓縮/000016.jpg');  
+      });
+
+      $("#p000021").hover(function(){  
+        $(this).attr('src','./colorful_img/500t/壓縮/000021.jpg');  
+        }, function(){  
+        $(this).attr('src','./flim_img/500t/壓縮/000021.jpg');  
+      });
+ 
+      $("#p000024").hover(function(){  
+        $(this).attr('src','./colorful_img/500t/壓縮/000024.jpg');  
+        }, function(){  
+        $(this).attr('src','./flim_img/500t/壓縮/000024.jpg');  
+      });
+
+      $("#p000025").hover(function(){  
+        $(this).attr('src','./colorful_img/500t/壓縮/000025.jpg');  
+        }, function(){  
+        $(this).attr('src','./flim_img/500t/壓縮/000025.jpg');  
+      });
+
+      $("#p000036").hover(function(){  
+        $(this).attr('src','./colorful_img/500t/壓縮/000036.jpg');  
+        }, function(){  
+        $(this).attr('src','./flim_img/500t/壓縮/000036.jpg');  
+      });
+
+      $("#p000037").hover(function(){  
+        $(this).attr('src','./colorful_img/500t/壓縮/000037.jpg');  
+        }, function(){  
+        $(this).attr('src','./flim_img/500t/壓縮/000037.jpg');  
+      });
+      
+});
+
+
+{/* <img class="img1_size" id="" src="./flim_img/koda/壓縮/000002.jpg" alt="">
+<img class="img1_size" id="" src="./flim_img/koda/壓縮/000018.jpg" alt="">
+<img class="img1_size" id="" src="./flim_img/koda/壓縮/000019.jpg" alt="">
+<img class="img1_size" id="" src="./flim_img/koda/壓縮/000020.jpg" alt="">
+<img class="img1_size" id="" src="./flim_img/koda/壓縮/000023.jpg" alt="">
+<img class="img1_size" id="" src="./flim_img/koda/壓縮/000026.jpg" alt="">
+<img class="img1_size" id="" src="./flim_img/koda/壓縮/000030.jpg" alt="">
+<img class="img1_size" id="" src="./flim_img/koda/壓縮/000032.jpg" alt="">
+<img class="img1_size" id="" src="./flim_img/koda/壓縮/000038.jpg" alt=""> */}
